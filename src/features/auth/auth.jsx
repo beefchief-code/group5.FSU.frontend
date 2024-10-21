8ae2de14fdcb7341b00bd03862e0e33eda0bd676
beefchief <c.lang.1993@gmail.com>
2024-10-21 12:39:52 -0500

    finish auth.jsx

---
+++ src/features/auth/auth.jsx
@@ -0,0 +1,77 @@
+import { useState } from "react";
+import { useLoginMutation, useRegisterMustation } from "./authSlice";
+import { useNavigate } from "react-router-dom";
+
+/**
+ * LoginForm allows user to register or login
+ */
+
+function LoginForm() {
+  const navigate = useNavigate();
+
+  //swap between register and login
+  const [isLogin, setIsLogin] = useState(true);
+  const authAction = isLogin ? "Login" : "Register";
+  //register or login message
+  const altCopy = isLogin
+    ? "Register for an account here"
+    : "Already registered? Login here";
+
+  //mutate data for login/registration
+  const [login, { error: loginError }] = useLoginMutation();
+  const [register, { error: registerError }] = useRegisterMustation();
+
+  //email and password to state
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  //send user email/password to api
+  const attemptAuth = async (event) => {
+    event.preventDefault();
+
+    //login or register
+    const loginMethod = isLogin ? login : register;
+    const creds = { email, password };
+
+    try {
+      await loginMethod(creds).unwrap();
+      navigate("/");
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  return (
+    <>
+      <h1>{authAction}</h1>
+      <form onSubmit={attemptAuth}>
+        <label>
+          Email
+          <input
+            type="email"
+            value={email}
+            onChange={(event) => setEmail(eevent.target.value)}
+            autoComplete="email"
+          />
+        </label>
+        <label>
+          Password
+          <input
+            type="password"
+            value={password}
+            onChange={(event) => setPassword(event.target.value)}
+            autoComplete="current-password"
+          />
+        </label>
+        <button>{authAction}</button>
+      </form>
+      <a href="#" onClick={() => setIsLogin(!isLogin)}>
+        {altCopy}
+      </a>
+      {isLogin && loginError && <p role="alert">{loginError}</p>}
+      {!islogin && registerError && <p role="alert">{registerError}</p>}
+    </>
+  );
+}
+
+export default LoginForm;
