c1a4e91357d8ec9e4ae51035d9081a6299d7d47e
wangbertha <wangbertha@gmail.com>
2024-10-23 15:37:08 -0500

    Create form for admin to add new professor

--- src/features/professors/Professors.jsx
+++ src/features/professors/Professors.jsx
@@ -1,9 +1,44 @@
-import { useGetProfessorsQuery } from "./professorSlice";
+import { useAddProfessorMutation, useGetProfessorsQuery } from "./professorSlice";
 import { NavLink } from "react-router-dom";
 import "./Professors.css";
+import { useState } from "react";
+import { useSelector } from "react-redux";
+import { selectToken } from "../auth/authSlice";
 
 export default function Professors() {
+    // Table Data
     const { data: professors = [], isLoading, error } = useGetProfessorsQuery();
+    
+    // Edit-Mode Controls
+    const [isEditing, setIsEditing] = useState(false);
+    const token = useSelector(selectToken);
+
+    // Form Input States
+    const [name, setName] = useState("");
+    const [profileImage, setProfileImage] = useState("");
+    const [bio, setBio] = useState("");
+    const [email, setEmail] = useState("");
+    const [phoneNumber, setPhoneNumber] = useState("");
+    const [departmentId, setDepartmentId] = useState("");
+
+    const [addProfessor] = useAddProfessorMutation();
+    const [addError, setAddError] = useState("");
+    async function sendAddProfessor(e) {
+        e.preventDefault();
+
+        try {
+            const response = await addProfessor({
+                name, profileImage, bio, email, phoneNumber, departmentId
+            });
+            if (!response) {
+                setAddError(response.error);
+            } else {
+                setIsEditing(false);
+            }
+        } catch (e) {
+            setAddError(e);
+        }
+    }
 
     if (isLoading) {
         return <p>Loading Professors...</p>;
@@ -36,6 +71,58 @@ export default function Professors() {
                     })}
                 </tbody>
             </table>
+            {token &&
+                <button onClick={() => setIsEditing(!isEditing)}>{!isEditing ? "Edit Mode" : "View Mode"}</button>
+            }
+            {isEditing &&
+                <form onSubmit={sendAddProfessor}>
+                    <h2>Add Professor</h2>
+                    <label> Name
+                        <input
+                            type="text"
+                            value={name}
+                            onChange={(e) => setName(e.target.value)}
+                        />
+                    </label>
+                    <label> Profile Picture
+                        <input
+                            type="text"
+                            value={profileImage}
+                            onChange={(e) => setProfileImage(e.target.value)}
+                        />
+                    </label>
+                    <label> Biography
+                        <input
+                            type="text"
+                            value={bio}
+                            onChange={(e) => setBio(e.target.value)}
+                        />
+                    </label>
+                    <label> Email
+                        <input
+                            type="text"
+                            value={email}
+                            onChange={(e) => setEmail(e.target.value)}
+                        />
+                    </label>
+                    <label> Phone Number
+                        <input
+                            type="text"
+                            value={phoneNumber}
+                            onChange={(e) => setPhoneNumber(e.target.value)}
+                        />
+                    </label>
+                    <label> Department Id
+                        <input
+                            type="number"
+                            value={departmentId}
+                            onChange={(e) => setDepartmentId(+e.target.value)}
+                        />
+                    </label>
+                    <button type="submit">Add Professor</button>
+                    {addError && <p>{addError.message}</p>}
+                </form>
+            }
         </main>
     )
 }
