6e5edb1cae1669182b0cac5fda624b1073b2f7e4
wangbertha <wangbertha@gmail.com>
2024-10-23 14:36:50 -0500

    Create form for admin to update professor

--- src/features/professors/Professor.jsx
+++ src/features/professors/Professor.jsx
@@ -1,55 +1,75 @@
 import { useParams } from "react-router-dom"
 
 // React-Redux
-import { useGetProfessorQuery } from "./professorSlice"
+import { useGetProfessorQuery, useUpdateProfessorMutation } from "./professorSlice"
 import { useSelector } from "react-redux";
 import { selectToken } from "../auth/authSlice";
 
 // Styling
 import "./Professor.css"
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 export default function Professor() {
+    // Initial Rendering
     const { id } = useParams();
-    const { data: professor, isLoading, error } = useGetProfessorQuery(id);
-    const [isEditing, setIsEditing] = useState(false);
+    const { data: professor, isLoading: professorLoading, error } = useGetProfessorQuery(id);
 
+    // Edit-Mode Controls
     const token = useSelector(selectToken);
+    const [isEditing, setIsEditing] = useState(false);
+    
+    // Form Input States
+    const [name, setName] = useState("");
+    const [profileImage, setProfileImage] = useState("");
+    const [bio, setBio] = useState("");
+    const [email, setEmail] = useState("");
+    const [phoneNumber, setPhoneNumber] = useState("");
+    const [departmentId, setDepartmentId] = useState("");
 
-    if (isLoading) {
-        return <p>Loading professor...</p>;
+    useEffect(() => {
+        setName(professor?.name);
+        setProfileImage(professor?.profileImage);
+        setBio(professor?.bio);
+        setEmail(professor?.email);
+        setPhoneNumber(professor?.phoneNumber);
+        setDepartmentId(professor?.departmentId);
+    }, [professor])
+
+    // Update Methods
+    const [updateProfessor] = useUpdateProfessorMutation();
+    const [updateError, setUpdateError] = useState("");
+    async function sendUpdateProfessor(e) {
+        e.preventDefault();
+
+        try {
+            const response = await updateProfessor({ 
+                id, 
+                name,
+                profileImage,
+                bio,
+                email, 
+                phoneNumber,
+                departmentId,
+            });
+            if (!response.error) {
+                setIsEditing(false);
+            } else {
+                setUpdateError(response.error);
+            }
+        } catch (e) {
+            setUpdateError(e);
+        }
     }
 
+    // Error Message Rendering
+    if (professorLoading) {
+        return <p>Loading professor...</p>;
+    }
     if (error) {
         return <p>{error.message}</p>;
     }
-
     if (!professor) {
         return <p>This professor is either not in our directory, or we ran into an error.</p>;
-    }
-
-    if (isEditing) {
-        return (
-            <main className="professor">
-                <form>
-                    <label> Name
-                        <input type="text" />
-                    </label>
-                    <label> Profile Picture
-                        <input type="text" />
-                    </label>
-                    <label> Biography
-                        <input type="text" />
-                    </label>
-                    <label> Email
-                        <input type="email" />
-                    </label>
-                    <label> Phone Number
-                        <input type="tel" />
-                    </label>
-                </form>
-            </main>
-        )
     }
 
     return (
@@ -62,6 +82,60 @@ export default function Professor() {
             <h3 className="contact-info">Contact Information</h3>
             <p>{professor.email}</p>
             <p>{professor.phoneNumber}</p>
+            {token && 
+                <button onClick={() => setIsEditing(!isEditing)}>
+                    {!isEditing ? "Edit" : "Exit Editing"}
+                </button>
+            }
+            {isEditing &&
+                <form onSubmit={sendUpdateProfessor}>
+                    <h1>Update Professor Information:</h1>
+                    <label> Name
+                        <input
+                            type="text"
+                            value={name}
+                            onChange={(e) => setName(e.target.value)}
+                        />
+                    </label>
+                    <label> Profile Picture
+                        <input
+                            type="text"
+                            value={profileImage}
+                            onChange={(e) => setProfileImage(e.target.value)}
+                        />
+                    </label>
+                    <label> Biography
+                        <input
+                            type="text"
+                            value={bio}
+                            onChange={(e) => setBio(e.target.value)}
+                        />
+                    </label>
+                    <label> Email
+                        <input
+                            type="text"
+                            value={email}
+                            onChange={(e) => setEmail(e.target.value)}
+                        />
+                    </label>
+                    <label> Phone Number
+                        <input
+                            type="text"
+                            value={phoneNumber}
+                            onChange={(e) => setPhoneNumber(e.target.value)}
+                        />
+                    </label>
+                    <label> Department Id
+                        <input
+                            type="number"
+                            value={departmentId}
+                            onChange={(e) => setDepartmentId(e.target.value)}
+                        />
+                    </label>
+                    <button type="submit">Update Professor</button>
+                    {updateError && <p>{updateError.message}</p>}
+                </form>
+            }
         </main>
     )
 }
