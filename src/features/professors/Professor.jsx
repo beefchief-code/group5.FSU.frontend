c4c6a19dc7e377680b92033404299da10c7e3f54
wangbertha <wangbertha@gmail.com>
2024-10-23 11:47:29 -0500

    Style Professor.jsx

--- src/features/professors/Professor.jsx
+++ src/features/professors/Professor.jsx
@@ -1,9 +1,20 @@
 import { useParams } from "react-router-dom"
+
+// React-Redux
 import { useGetProfessorQuery } from "./professorSlice"
+import { useSelector } from "react-redux";
+import { selectToken } from "../auth/authSlice";
+
+// Styling
+import "./Professor.css"
+import { useState } from "react";
 
 export default function Professor() {
     const { id } = useParams();
     const { data: professor, isLoading, error } = useGetProfessorQuery(id);
+    const [isEditing, setIsEditing] = useState(false);
+
+    const token = useSelector(selectToken);
 
     if (isLoading) {
         return <p>Loading professor...</p>;
@@ -17,12 +28,37 @@ export default function Professor() {
         return <p>This professor is either not in our directory, or we ran into an error.</p>;
     }
 
+    if (isEditing) {
+        return (
+            <main className="professor">
+                <form>
+                    <label> Name
+                        <input type="text" />
+                    </label>
+                    <label> Profile Picture
+                        <input type="text" />
+                    </label>
+                    <label> Biography
+                        <input type="text" />
+                    </label>
+                    <label> Email
+                        <input type="email" />
+                    </label>
+                    <label> Phone Number
+                        <input type="tel" />
+                    </label>
+                </form>
+            </main>
+        )
+    }
+
     return (
-        <main>
+        <main className="professor">
             <h1>{professor.name}</h1>
+            <p className="department-name">{professor.department.name}</p>
             <img src={professor.profileImage} alt="Professor profile picture" />
+            <h2>Biography:</h2>
             <p>{professor.bio}</p>
-            <p>Department: {professor.department.name}</p>
             <h3 className="contact-info">Contact Information</h3>
             <p>{professor.email}</p>
             <p>{professor.phoneNumber}</p>
