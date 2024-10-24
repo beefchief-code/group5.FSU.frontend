7546ddf40d15a9ed08ec81b53a6b0bc6418a83ba
Rogelio Martinez <rogejr89@gmail.com>
2024-10-23 21:56:18 -0400

    Created new departments.css file for styling. Added html code to Departments.jsx & Department.jsx

--- src/features/departments/Departments.jsx
+++ src/features/departments/Departments.jsx
@@ -1,5 +1,6 @@
+import { Link } from "react-router-dom";
 import { useGetDepartmentsQuery } from "./departmentSlice"; 
-import DepartmentRow from "./DepartmentRow";
+import './departments.css'
 
 
 export default function Departments() { 
@@ -7,35 +8,35 @@ export default function Departments() {
     const { data: departments = [], isLoading, error } = useGetDepartmentsQuery(); 
 
     if (isLoading) {
-        return <p>Loading Departments...</p>;
+        return <p>Loading departments...</p>;
     }
     if (error) {
         return <p>{error.error}: {error.message}</p>;
     }
     if (!departments.length) {
-        return <p>There are no Departments.</p>;
+        return <p>There are no departments.</p>;
     }
 
     return (
         <>
-            <table>
-                <thead>
-                    <tr>
-                        <th><h2>Contact List</h2></th>
-                    </tr>
-                </thead>
-                <tbody>
-                    <tr>
-                        <th>Departments Name</th>
-                        <th>Description</th>
-                        <th>Email</th>
-                        <th>Phone</th>
-                    </tr>
+            <header className="departments-component-header">
+                <h1>Academic Departments</h1>
+                <h3>Connect With Our Academic Departments</h3>
+            </header>
+            <section className="departments-component-section">
+                <ul className="departments-flex-box">
                     {
-                        departments.map((department) => ( <DepartmentRow key={department.id} department={department} /> )) 
+                        departments.map((department) => (
+                            <li>
+                                <Link to={`/departments/${department.id}`} >
+                                    <img src={department.image} alt={department.name} />
+                                    <h2>{department.name}</h2>
+                                </Link>
+                            </li>
+                        ))
                     }
-                </tbody>
-            </table>
+                </ul>
+            </section>
         </>
     )
 }
